04862ea5da545b0a5f4109f0c20d42323327c6f7
Adam Arutyunov <adam.arut@gmail.com>
2019-11-30 17:06:37 +0300

    Finished to develop basic moves. You can test it by running Game.py

--- Game.py
+++ Game.py
@@ -4,18 +4,14 @@ from base import *
 
 class Game:
     def __init__(self):
-        pygame.init()
-
-        self.w, self.h = 500, 500
+        self.w, self.h = 1000, 500
         self.size = (self.w, self.h)
-        self.screen = pygame.display.set_mode(size)
+        self.screen = pygame.display.set_mode(self.get_size())
         
-        self.player = Player()
-        self.object.append(self.player)
+        self.objects = []
 
-        self.keys_down = set()
+        self.keys = []
         
-    @property
     def get_size(self):
         return self.size
 
@@ -23,8 +19,10 @@ class Game:
         self.objects.append(obj)
 
     def draw(self):
+        self.screen.fill((0, 0, 0))
         for obj in self.objects:
-            obj.draw()
+            obj.draw(self.screen)
+        pygame.display.flip()
 
     def close(self):
         pygame.quit()
@@ -32,24 +30,42 @@ class Game:
     def update(self):
         events = pygame.event.get()
         for event in events:
-            if event.type is pygame.KEYDOWN:
-                self.keys_down.add(event.key)
-            elif event.type is pygame.KEYUP:
-                self.keys_down.discard(event.key)
+            if event.type is pygame.QUIT:
+                self.close()
 
         self.handle_shortcuts()
         for obj in self.objects:
             obj.update()
 
     def handle_shortcuts(self):
-        self.player.update_vector()
+        self.keys = pygame.key.get_pressed()
+        x_speed = 0
+        y_speed = 0
+        if self.keys[pygame.K_LEFT]:
+            x_speed -= 1
+        if self.keys[pygame.K_RIGHT]:
+            x_speed += 1
+        if self.keys[pygame.K_UP]:
+            y_speed -= 1
+        if self.keys[pygame.K_DOWN]:
+            y_speed += 1
+            
+        player.x_delta = x_speed
+        player.y_delta = y_speed
         
 
+pygame.init()
+clock = pygame.time.Clock()
+fps = 60
 
 UGame = Game()
-screen = pygame.display.set_mode(UGame.get_size())
+player = Player(100, 100)
 
+UGame.spawn_object(player)
 while True:
     UGame.update()
     UGame.draw()
+    clock.tick(fps)
+
+pygame.quit()
     
